99c3b3eda628f7abb55a9b36c9a8a97c640ec111
jnt02371 <70246688+jnt02371@users.noreply.github.com>
2020-10-26 12:52:03 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,29 @@
+function sendchris(){
+	inputName = document.getElementById("name").value;
+	window.location = 'http://wa.me/?text='+inputName+' wishing you for the year 2020 %0A%0Ahttps://jnt02371.github.io/TT1/Christmas.html';
+}
+function sendraya(){
+	inputName = document.getElementById("name").value;
+	window.location = 'http://wa.me/?text='+inputName+' wishing you for the year 2020 %0A%0Ahttps://jnt02371.github.io/TT1/Christmas.html';
+}
+function senddeepavali(){
+	inputName = document.getElementById("name").value;
+	window.location = 'http://wa.me/?text='+inputName+' wishing you for the year 2020 %0A%0Ahttps://jnt02371.github.io/TT1/Christmas.html';
+}
+function sendcny(){
+	inputName = document.getElementById("name").value;
+	window.location = 'http://wa.me/?text='+inputName+' wishing you for the year 2020 %0A%0Ahttps://jnt02371.github.io/TT1/Christmas.html';
+}
+function requestFullScreen(element) {
+    // Supports most browsers and their versions.
+    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+
+    if (requestMethod) { // Native full screen.
+        requestMethod.call(element);
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+}
