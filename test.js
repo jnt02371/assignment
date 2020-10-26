c03ba687d3cb6a598f996fc6d1ca1e765704696c
jnt02371 <70246688+jnt02371@users.noreply.github.com>
2020-10-26 12:36:24 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,4 @@
+function sendchristmastowhatsapp(){
+	inputName = docuent.getElementById("name").value;
+	window.location = 'http://wa.me/?text='+inputVal+' wishing you for the year 2020 %0A%0Ahttps://jnt02371.github.io/TT1/Christmas.html';
+}
