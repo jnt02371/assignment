4f0725cce6da82e43525d60a0124e23eb08d7ee0
jnt02371 <70246688+jnt02371@users.noreply.github.com>
2020-10-26 11:59:49 +0800

    Add files via upload

---
+++ foralluse.js
@@ -0,0 +1,37 @@
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+function getmytext() {
+	var mytext = getUrlVars()["text"];
+	alert(mytext);
+}
+
+function sendtowhatsapp()
+{
+var mytext = getUrlVars()["text"];
+	var inputVal = document.getElementById("textboxid").value;
+	var outputVal = inputVal.replace(" ", "~");
+	var outputVal2 = outputVal.replace(" ", "~");
+	var outputVal3 = outputVal2.replace(" ", "~");
+	var outputVal4 = outputVal3.replace(" ", "~");
+window.location = 'http://wa.me/?text='+inputVal+' wishing you a Happy Chinese New Year 2020 %0A%0AOpen this ðŸ‘‡ %0A%0Adcs5604.com/cny2020.html?name='+outputVal4;
+}
+
+function requestFullScreen(element) {
+    // Supports most browsers and their versions.
+    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+
+    if (requestMethod) { // Native full screen.
+        requestMethod.call(element);
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+}
