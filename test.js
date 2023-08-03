33bab6f3c7041f1489d1848e28cdb57ecaa7f1ce
jnt02371 <70246688+jnt02371@users.noreply.github.com>
2023-08-03 19:20:01 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,18 @@
 function sendchris(){
 	inputName = document.getElementById("name").value;
-	window.location = 'http://wa.me/?text='+inputName+' wishing you a Merry Christmas for the year 2020 %0A%0Ahttps://jnt02371.github.io/assignment/Christmas.html';
+	window.location = 'http://wa.me/?text='+inputName+' is wishing you a Merry Christmas for the year 2020 %0A%0Ahttps://jnt02371.github.io/assignment/Christmas.html';
 }
 function sendraya(){
 	inputName = document.getElementById("name").value;
-	window.location = 'http://wa.me/?text='+inputName+' wishing you a Hari Raya for the year 2020 %0A%0Ahttps://jnt02371.github.io/assignment/HariRaya.html';
+	window.location = 'http://wa.me/?text='+inputName+' is wishing you a Hari Raya for the year 2020 %0A%0Ahttps://jnt02371.github.io/assignment/HariRaya.html';
 }
 function senddeepavali(){
 	inputName = document.getElementById("name").value;
-	window.location = 'http://wa.me/?text='+inputName+' wishing you a happy Deepavali for the year 2020 %0A%0Ahttps://jnt02371.github.io/assignment/deepavali1.html';
+	window.location = 'http://wa.me/?text='+inputName+' is wishing you a happy Deepavali for the year 2020 %0A%0Ahttps://jnt02371.github.io/assignment/deepavali1.html';
 }
 function sendcny(){
 	inputName = document.getElementById("name").value;
-	window.location = 'http://wa.me/?text='+inputName+' wishing you a happy Chinese New Year for the year 2020 %0A%0Ahttps://jnt02371.github.io/assignment/cny.html';
+	window.location = 'http://wa.me/?text='+inputName+' is wishing you a happy Chinese New Year for the year 2020 %0A%0Ahttps://jnt02371.github.io/assignment/cny.html';
 }
 function requestFullScreen(element) {
     // Supports most browsers and their versions.
